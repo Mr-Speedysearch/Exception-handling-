7b0cd66e296828391c9fbe96f05a4c8a4f1003fa
SpeedyGonzalez <52254253+Mr-Speedysearch@users.noreply.github.com>
2025-02-14 15:24:59 +0000

    Add files via upload

--- StudentPojo.java
+++ StudentPojo.java
@@ -25,5 +25,42 @@ public class StudentPojo {
                 ", classList='" + classList + '\'' +
                 '}';
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return DateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        DateOfBirth = dateOfBirth;
+    }
+
+    public String getClassList() {
+        return classList;
+    }
+
+    public void setClassList(String classList) {
+        this.classList = classList;
+    }
 }
 
+/* Pojo is a class with attributes and its getters and setters. boilerplate code
+    However we can also use the *Record* Type, to replace boilerplate code and be more restrictive "plain data carriers"
+    A record is a special class with data that cant be altered, it only contains methods like constructors.
+    Record code doesn't need to be generated.
+ */
