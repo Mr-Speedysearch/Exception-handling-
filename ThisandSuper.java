74732022d0240b8cfc0d9fba5d8c6354c303a39c
SpeedyGonzalez <52254253+Mr-Speedysearch@users.noreply.github.com>
2025-03-18 14:29:28 +0000

    Add files via upload

---
+++ ThisandSuper.java
@@ -0,0 +1,12 @@
+public class ThisandSuper {
+
+    /*
+   The keyword "Super" is used to access the parent class but can also be used for method overriding when the method
+   has the same name.
+
+       whereas the keyword "this" is used to call the current class which is used when we have the same parameter and instance
+        variable or field.
+        With "this" you can also do constructor chaining.
+     */
+
+}
